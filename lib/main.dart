711b42b31f0d436fe3e689692919b07fdd63235d
Jam12390 <garnettjames44@gmail.com>
2024-11-05 23:29:38 +0000

    Initial Scaffolding Of App

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,117 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Project Manager',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ProjectList(title: 'Project List'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+//will change to homepage later on - could add todo list to homepage??
+class ProjectList extends StatefulWidget {
+  const ProjectList({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ProjectList> createState() => ProjectListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ProjectListState extends State<ProjectList> {
+  //making the default page0 so it doesnt crash off of startup
+  Widget page = const Page0();
 
-  void _incrementCounter() {
+  //Text style for all text on this page
+  final txtStyle = const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  //function to switch page using index from the list tile
+  void SwitchIndex(int index){
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      switch(index){
+        case 0:
+        page = const Page0();
+        case 1:
+        page = const Page1();
+      }
     });
+    //close drawer
+    Navigator.pop(context);
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Text("Project List"),
+        backgroundColor: Colors.red,
+        //adding drawer icon to appbar
+        leading: Builder(
+          builder: (context){
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              }
+            );
+          }
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Drawer(
+        child: SafeArea(
+          //list projects here - maybe automate list when implementing files - like read the number of projects, what they are, etc and list them here
+          child: ListView(
+            children: [
+              ListTile(
+                title: const Text("index 0"),
+                //did not know that i had to do this - prevents function being called on build which if it is called raises an error :l
+                  onTap: () => SwitchIndex(0),
+              ),
+              ListTile(
+                title: const Text("index 1"),
+                onTap: () => SwitchIndex(1),
+              ),
+            ],
+          )
+        ),
+      ),
+      body: page
+    );
+  }
+}
+
+//test pages to make sure initial page swapping actually works - it does :D
+class Page0 extends StatelessWidget{
+  const Page0({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Center(
+        child: Text("Index 0 - Page 0"),
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget{
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Center(
+        child: Text("Index 1 - Page 1"),
+      ),
     );
   }
 }
